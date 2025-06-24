ed10002ac982873c7f43f76a797c75ea3c56c7cd
Ксения Шлёнская <k.shlenskaya@duotek.ru>
2025-06-24 21:57:52 +0300

    add environment config and requirements

---
+++ app/config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
+SPOTIFY_REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI')
+DATABASE_URL = os.getenv('DATABASE_URL')
